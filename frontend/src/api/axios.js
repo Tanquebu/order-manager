e92eacbed4400ef5b35fd94955e9cb3cb98feaee
Massimiliano Nicosia <massimiliano.nicosia@gmail.com>
2025-04-05 12:43:04 +0200

    added cookies/cors for Sanctum

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'http://localhost/api',
+  withCredentials: true,    
   timeout: 10000,        // 10 secondi max di timeout per eventuali richieste lente
   headers: {
     'Content-Type': 'application/json',
