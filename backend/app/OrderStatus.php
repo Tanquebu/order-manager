788a7dd32bacca83d1a220702ef9f69ef18db908
Massimiliano Nicosia <massimiliano.nicosia@gmail.com>
2025-04-05 10:20:04 +0200

    added orderStatus enums

---
+++ backend/app/OrderStatus.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+enum OrderStatus: string
+{
+    case NEW = 'new';
+    case PROCESSING = 'processing';
+    case PENDING = 'pending';
+    case PAYED = 'payed';
+    case PAYMENT_REVIEW = 'payment_review';
+    case REFUNDED = 'refunded';
+    case SHIPPED = 'shipped';
+    case DELIVERED = 'delivered';
+    case CANCELED = 'canceled';
+    case CLOSED = 'closed';
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+}
