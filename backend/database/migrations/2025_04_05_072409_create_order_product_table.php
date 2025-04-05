d13e5b40e61b00b3c14b5555985710b8d578d39f
Massimiliano Nicosia <massimiliano.nicosia@gmail.com>
2025-04-05 10:17:14 +0200

    added entities and migrations

---
+++ backend/database/migrations/2025_04_05_072409_create_order_product_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->constrained()->onDelete('cascade');
+            $table->enum('status', ['new','processing', 'pending', 'payed', 'shipped', 'delivered', 'canceled', 'closed'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_product');
+    }
+};
